70764e8fbd20e94c7763e38e164b0341f001c5d2
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-09-19 07:24:16 +0530

    left half triangle

--- loops/pattern.c
+++ loops/pattern.c
@@ -1,8 +1,11 @@
 // //rectangle patterns
-#include<stdio.h>
-int main(){
-  for(int i = 1 ; i <= 5 ; i++){
-    for(int j = 1 ; j <= 5 ; j++){
+#include <stdio.h>
+int main()
+{
+  for (int i = 1; i <= 5; i++)
+  {
+    for (int j = 1; j <= 5; j++)
+    {
       printf("*");
     }
     printf("\n");
@@ -10,11 +13,14 @@ int main(){
   return 0;
 }
 
-//right-half triangle
-#include<stdio.h>
-int main(){
-  for(int i = 1 ; i <= 5 ; i++){
-    for(int j = 1 ; j <= i ; j++){
+// right-half triangle
+#include <stdio.h>
+int main()
+{
+  for (int i = 1; i <= 5; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
       printf("*");
     }
     printf("\n");
@@ -22,27 +28,33 @@ int main(){
   return 0;
 }
 
-//right-half triangle print alphabets
-#include<stdio.h>
-int main(){
+// right-half triangle print alphabets
+#include <stdio.h>
+int main()
+{
   int n;
   printf("enter a number : ");
-  scanf("%d",&n);
+  scanf("%d", &n);
   char a = 'A';
-  for(int i = 0 ; i <= n ; i++){
-    for(int j = 1 ; j <= i ; j++){
-      printf("%c",a++);
+  for (int i = 0; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      printf("%c", a++);
     }
     printf("\n");
   }
   return 0;
 }
 
-//inverted Right Half Pyramid Pattern in C
-#include<stdio.h>
-int main(){
-  for(int i = 5 ; i >=0 ; i--){
-    for(int j = 1 ; j <= i ; j++){
+// inverted Right Half Pyramid Pattern in C
+#include <stdio.h>
+int main()
+{
+  for (int i = 5; i >= 0; i--)
+  {
+    for (int j = 1; j <= i; j++)
+    {
       printf("*");
     }
     printf("\n");
@@ -50,6 +62,24 @@ int main(){
   return 0;
 }
 
-
-
-
+// left half triangle
+//  Online C compiler to run C program online
+#include <stdio.h>
+int main()
+{
+  int n;
+  printf("enter a number : ");
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n; j++)
+    {
+      if ((i + j) <= n)
+        printf(" ");
+      else
+        printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
