3451ab932eb6cfd55d34b7b06c7467e51ee269e2
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-10-22 04:36:24 +0530

    2d arrays

--- arrays/2Darray.c
+++ arrays/2Darray.c
@@ -1,18 +1,42 @@
+//taking inputs and printing elements in 2D arrays
 #include<stdio.h>
 
 int main() {
-    int arr[2][3]; // Correct size for the array
+    int arr[2][3]; 
     // Input elements
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
             scanf("%d", &arr[i][j]);
         }
     }
-    // Output elements
+    // Output 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
             printf("%d\n", arr[i][j]);
         }
     }
+    return 0;
+}
+
+
+//taking input from the user and find the total of elements present in 2D arrays
+#include <stdio.h>
+int main(){
+    int arr[2][5],sum = 0;
+    //taking input from the user
+    for(int i = 0 ; i < 2 ; i++){
+        for(int j = 0 ; j < 5 ; j++){
+            printf("enter elements : ");
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     for(int i = 0 ; i < 2 ; i++){
+        for(int j = 0 ; j < 5 ; j++){
+            printf("%d\t",arr[i][j]);
+            sum += arr[i][j];
+        }
+        printf("\n");
+    }
+    printf("total sum is : %d", sum);
     return 0;
 }
