a0c40a529f27096b93d43ee68c658366fb5160a2
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-10-23 12:38:26 +0530

    pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -4,7 +4,7 @@
 int main() {
     int a = 10 , c ; 
     int *p;
-    p = &a; //stroing the address of a in p
+    p = &a; //storing the address of a in p
     c = *p;
     //this will print the value of a
     printf("%d\n",a);
@@ -21,5 +21,25 @@ int main() {
     printf("%d",c);
     
 
+    return 0;
+}
+
+//fetching pointer address through arrays
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *p = &arr[0]; 
+    int *q = &arr[1]; 
+    int *r = &arr[2]; 
+    int *s = &arr[3]; 
+    int *t = &arr[4]; 
+    printf("%x\n",p);
+    printf("%x\n",q);
+    printf("%x\n",r);
+    printf("%x\n",s);
+    printf("%x\n",t);
+    
+    
     return 0;
 }
