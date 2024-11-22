dc896e2259f0470b1b467deb0f6f7f509d1edc59
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-11-22 08:41:40 +0530

    null pointers

--- pointers/nullPointers.c
+++ pointers/nullPointers.c
@@ -10,6 +10,7 @@ int main(){
     printf("%d\n",*ptr);
   }
   //this is a way to declare a null pointer 
+  
   // printf("%d\n",ptr); //0
   // printf("%d\n",*ptr); //will throw error or system will crash , because we cant dereference the null pointer
   // printf("%d\n",ptr2); //will throw garbage value
