ff26e4fa4e9275e489585c747b5d8890b34c5a99
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-12-21 23:15:19 +0530

    structure

--- structure/structurePointer.c
+++ structure/structurePointer.c
@@ -10,3 +10,23 @@ void main(){
   printf("The information for s is:\n");
   printf("Roll number is %d, Name is %s, Marks is %f\n", ptr->rollNumber, ptr->name, ptr->marks);
 }
+
+
+#include <stdio.h>
+
+typedef struct student {
+    char fname[20];
+    char lname[20];
+    int marks;
+} stu;
+
+void main() {
+    stu u;
+    printf("Enter first name : ");
+    scanf("%s",u.fname);
+    printf("Enter last name : ");
+    scanf("%s",u.lname);
+    printf("Enter marks : ");
+    scanf("%d",&u.marks);
+    printf("Full name is %s %s, and marks is %d\n", u.fname, u.lname, u.marks);
+}
