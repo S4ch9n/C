6c9040015215c97bd1315c86037d63a8487bc6c5
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-12-22 21:13:13 +0530

    structure

--- structure/structurePointer.c
+++ structure/structurePointer.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-struct student {
+struct student1 {
   int rollNumber;
   char name[20];
   float marks;
 };
 void main(){
-  struct student s = {1 , "jerry" , 90};
-  struct student *ptr = &s;
+  struct student1 s = {1 , "jerry" , 90};
+  struct student1 *ptr = &s;
   printf("The information for s is:\n");
   printf("Roll number is %d, Name is %s, Marks is %f\n", ptr->rollNumber, ptr->name, ptr->marks);
 }
