ce6567951937e68009188433d6a9589dffd00437
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-01-01 07:38:35 +0530

    tail.c

--- recursion/tail.c
+++ recursion/tail.c
@@ -1,3 +1,4 @@
+//A tail-recursive function is one where the recursive call is the last operation performed by the function. There are no further computations or statements after the recursive call. This allows for certain optimizations, such as reusing the same stack frame for multiple calls.
 #include<stdio.h>
 void tail(int a){
   if(a < 1) return;
